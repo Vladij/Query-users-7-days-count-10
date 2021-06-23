3e3d7f1248ad90f46fa27a0990c29f5eabdb0694
Vladij <vladij01@gmail.com>
2021-06-23 11:19:33 +0300

    Update main.php

--- main.php
+++ main.php
@@ -4,6 +4,6 @@
             ->where('create_at', '>', Carbon::now()->subDays(7))
             ->select('item_id', DB::raw('COUNT(*) as users_count'))
             ->groupBy('item_id')
-            ->having('users_count', '>=', 2)->get();
+            ->having('users_count', '>=', 10)->get();
         return view('home', ['users' => $users]);
     }
